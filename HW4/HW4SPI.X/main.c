51a73592086daa6db644890e5f9d2c8dbc523ad2
Brian Soetikno <briansoetikno@Brians-MBP.cable.rcn.com>
2017-04-11 20:49:18 -0500

    Homework 4 working code

--- HW4/HW4SPI.X/main.c
+++ HW4/HW4SPI.X/main.c
@@ -1,7 +1,13 @@
 
+#include <proc/p32mx250f128b.h>
+#include <math.h>
 #include "PIC32Setup.h"
+#define CS LATAbits.LATA4
 
-unsigned char spi_io(unsigned char o) {
+float sineWaveform[200];
+float rampWaveform[200];
+
+unsigned char spi_io(unsigned short o) {
     SPI1BUF = o; // write 
     while (!SPI1STATbits.SPIRBF) {
         // wait until byte is received
@@ -25,13 +31,13 @@ unsigned char spi_io(unsigned char o) {
 // bits 4 through 11 contain the 8 bits of information
 // bits 0 through 3 are not used for 4902
 void setVoltage(char channel, float voltage) {
-    LATBbits.LATB8 = 0; // set the chip select pin, RA0, to low
+    CS = 0; // set the chip select pin, RA0, to low
     int voltage2 = voltage;
-    short out = (channel << 15) | 0x7000 | (voltage2 << 4);
+    unsigned short out = (channel << 15) | 0x7000 | (voltage2 << 4);
     
     spi_io(out);
     
-    LATBbits.LATB8 = 1; // stop the write
+    CS = 1; // stop the write
 }
 // Data Sheet Notes:
 // All writes to the MCP4902/4912/4922 are 16-bit words.
@@ -39,25 +45,32 @@ void setVoltage(char channel, float voltage) {
 //Most Significant 4 bits are Configuration bits. The
 //remaining 12 bits are data bits. 
 void spi_setup() {
-    // make A0 an output pin for chip select
-    TRISBbits.TRISB8 = 0; // acts as ss1
-    LATBbits.LATB8 = 1;     // we arent writing right now
+    // make A4 an output pin for chip select
+    TRISAbits.TRISA4 = 0;       // acts as ss1
+    CS = 1;                     // we arent writing right now
     // select the pins for spi communication
-    SS1Rbits.SS1R = 0b0011;  //  not used
-    SDI1Rbits.SDI1R = 0b0111; // not used
-    RPA1Rbits.RPA1R = 0b0011; // set output rpa1 (pin3) to sdo1
+    SS1Rbits.SS1R = 0b0011;     //  not used, just for practice
+    SDI1Rbits.SDI1R = 0b0111;   // not used, just for practice
+    RPA1Rbits.RPA1R = 0b0011;   // set output rpa1 (pin3) to sdo1
     SPI1CON = 0;
-    SPI1BUF;                // clear the SPI buffer by reading from it
-    SPI1BRG = 0x1;          // baud rate to 12 MHz 
-    SPI1STATbits.SPIROV = 0; // clear the overflow bit
+    SPI1BUF;                    // clear the SPI buffer by reading from it
+    SPI1BRG = 0x1000;           // baud rate. set to 0x1 for fastests
+    SPI1STATbits.SPIROV = 0;    // clear the overflow bit
     SPI1CONbits.MODE32 = 0;
-    SPI1CONbits.MODE16 = 1; // we'll need to send 16 bit words which contain 4 bits of config, and 8 bits of data, 4 bits unused
+    SPI1CONbits.MODE16 = 1;     // we'll need to send 16 bit words which contain 4 bits of config, and 8 bits of data, 4 bits unused
     SPI1CONbits.CKE = 1;   
-    SPI1CONbits.MSTEN = 1;  // master operation
-    SPI1CONbits.ON = 1;     // turn on SPI1
+    SPI1CONbits.MSTEN = 1;      // master operation
+    SPI1CONbits.ON = 1;         // turn on SPI1
     
 }
 
+void makeWaveforms() {
+    int i=0;
+    for (i=0; i < 200; i ++) {
+        sineWaveform[i] = 255.0/2.0*sin(2*3.14159*i/200) +255.0/2.0;
+        rampWaveform[i] = 255.0/200.0*i;
+    }
+}
 
 int main() {
 
@@ -78,20 +91,29 @@ DDPCONbits.JTAGEN = 0;
 // do your TRIS and LAT commands here
 __builtin_enable_interrupts();
 
-
-
 // set up SPI
 spi_setup();
 
-    while(1) {
-        if (_CP0_GET_COUNT() >= 24000) {
-            _CP0_SET_COUNT(0);
-            setVoltage(0,1);
-            setVoltage(1,2);
-        }
-    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+// make waveform arrays
+makeWaveforms();
 
-    // remember the core timer runs at half the sysclk
+int count =0;
+    while(1) {
+        _CP0_SET_COUNT(0);
+        
+        // 1 ms delay, sets the frequency of the sine wave
+        while(_CP0_GET_COUNT() < 24000) {
+            
+        }
+        
+        setVoltage(0,sineWaveform[count]);
+        setVoltage(1,rampWaveform[count]);
+        
+        count ++;
+        
+        if (count==199){
+            count = 0;
+        }
 
     }
     return 0;
