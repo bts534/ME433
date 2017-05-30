4ac00ba5fedef7024eec18bc313f03ff83880fcc
Brian Soetikno <brian.soetikno@northwestern.edu>
2017-05-30 13:39:51 -0500

    Finished HW8

--- HW8/HW8_2/firmware/HW8_2.X/nbproject/Makefile-default.mk
+++ HW8/HW8_2/firmware/HW8_2.X/nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../src/system_config/default/framework/system/clk/src/sys_clk_static.c ../src/system_config/default/framework/system/ports/src/sys_ports_static.c ../src/system_config/default/system_init.c ../src/system_config/default/system_interrupt.c ../src/system_config/default/system_exceptions.c ../src/system_config/default/system_tasks.c ../src/app.c ../src/main.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c i2c_master_noint.c ILI9163C.c
+SOURCEFILES_QUOTED_IF_SPACED=../src/system_config/default/framework/system/clk/src/sys_clk_static.c ../src/system_config/default/framework/system/ports/src/sys_ports_static.c ../src/system_config/default/system_init.c ../src/system_config/default/system_interrupt.c ../src/system_config/default/system_exceptions.c ../src/system_config/default/system_tasks.c ../src/app.c ../src/main.c i2c_master_noint.c ILI9163C.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/639803181/sys_clk_static.o ${OBJECTDIR}/_ext/822048611/sys_ports_static.o ${OBJECTDIR}/_ext/1688732426/system_init.o ${OBJECTDIR}/_ext/1688732426/system_interrupt.o ${OBJECTDIR}/_ext/1688732426/system_exceptions.o ${OBJECTDIR}/_ext/1688732426/system_tasks.o ${OBJECTDIR}/_ext/1360937237/app.o ${OBJECTDIR}/_ext/1360937237/main.o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ${OBJECTDIR}/i2c_master_noint.o ${OBJECTDIR}/ILI9163C.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/639803181/sys_clk_static.o.d ${OBJECTDIR}/_ext/822048611/sys_ports_static.o.d ${OBJECTDIR}/_ext/1688732426/system_init.o.d ${OBJECTDIR}/_ext/1688732426/system_interrupt.o.d ${OBJECTDIR}/_ext/1688732426/system_exceptions.o.d ${OBJECTDIR}/_ext/1688732426/system_tasks.o.d ${OBJECTDIR}/_ext/1360937237/app.o.d ${OBJECTDIR}/_ext/1360937237/main.o.d ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d ${OBJECTDIR}/i2c_master_noint.o.d ${OBJECTDIR}/ILI9163C.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/639803181/sys_clk_static.o ${OBJECTDIR}/_ext/822048611/sys_ports_static.o ${OBJECTDIR}/_ext/1688732426/system_init.o ${OBJECTDIR}/_ext/1688732426/system_interrupt.o ${OBJECTDIR}/_ext/1688732426/system_exceptions.o ${OBJECTDIR}/_ext/1688732426/system_tasks.o ${OBJECTDIR}/_ext/1360937237/app.o ${OBJECTDIR}/_ext/1360937237/main.o ${OBJECTDIR}/i2c_master_noint.o ${OBJECTDIR}/ILI9163C.o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/639803181/sys_clk_static.o.d ${OBJECTDIR}/_ext/822048611/sys_ports_static.o.d ${OBJECTDIR}/_ext/1688732426/system_init.o.d ${OBJECTDIR}/_ext/1688732426/system_interrupt.o.d ${OBJECTDIR}/_ext/1688732426/system_exceptions.o.d ${OBJECTDIR}/_ext/1688732426/system_tasks.o.d ${OBJECTDIR}/_ext/1360937237/app.o.d ${OBJECTDIR}/_ext/1360937237/main.o.d ${OBJECTDIR}/i2c_master_noint.o.d ${OBJECTDIR}/ILI9163C.o.d ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/639803181/sys_clk_static.o ${OBJECTDIR}/_ext/822048611/sys_ports_static.o ${OBJECTDIR}/_ext/1688732426/system_init.o ${OBJECTDIR}/_ext/1688732426/system_interrupt.o ${OBJECTDIR}/_ext/1688732426/system_exceptions.o ${OBJECTDIR}/_ext/1688732426/system_tasks.o ${OBJECTDIR}/_ext/1360937237/app.o ${OBJECTDIR}/_ext/1360937237/main.o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ${OBJECTDIR}/i2c_master_noint.o ${OBJECTDIR}/ILI9163C.o
+OBJECTFILES=${OBJECTDIR}/_ext/639803181/sys_clk_static.o ${OBJECTDIR}/_ext/822048611/sys_ports_static.o ${OBJECTDIR}/_ext/1688732426/system_init.o ${OBJECTDIR}/_ext/1688732426/system_interrupt.o ${OBJECTDIR}/_ext/1688732426/system_exceptions.o ${OBJECTDIR}/_ext/1688732426/system_tasks.o ${OBJECTDIR}/_ext/1360937237/app.o ${OBJECTDIR}/_ext/1360937237/main.o ${OBJECTDIR}/i2c_master_noint.o ${OBJECTDIR}/ILI9163C.o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o
 
 # Source Files
-SOURCEFILES=../src/system_config/default/framework/system/clk/src/sys_clk_static.c ../src/system_config/default/framework/system/ports/src/sys_ports_static.c ../src/system_config/default/system_init.c ../src/system_config/default/system_interrupt.c ../src/system_config/default/system_exceptions.c ../src/system_config/default/system_tasks.c ../src/app.c ../src/main.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c i2c_master_noint.c ILI9163C.c
+SOURCEFILES=../src/system_config/default/framework/system/clk/src/sys_clk_static.c ../src/system_config/default/framework/system/ports/src/sys_ports_static.c ../src/system_config/default/system_init.c ../src/system_config/default/system_interrupt.c ../src/system_config/default/system_exceptions.c ../src/system_config/default/system_tasks.c ../src/app.c ../src/main.c i2c_master_noint.c ILI9163C.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c
 
 
 CFLAGS=
@@ -148,18 +148,6 @@ ${OBJECTDIR}/_ext/1360937237/main.o: ../src/main.c  nbproject/Makefile-${CND_CON
 	@${RM} ${OBJECTDIR}/_ext/1360937237/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/1360937237/main.o.d" -o ${OBJECTDIR}/_ext/1360937237/main.o ../src/main.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
-${OBJECTDIR}/_ext/2112325346/sys_devcon.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
-	
-${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
-	
 ${OBJECTDIR}/i2c_master_noint.o: i2c_master_noint.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/i2c_master_noint.o.d 
@@ -171,6 +159,18 @@ ${OBJECTDIR}/ILI9163C.o: ILI9163C.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/ILI9163C.o.d 
 	@${RM} ${OBJECTDIR}/ILI9163C.o 
 	@${FIXDEPS} "${OBJECTDIR}/ILI9163C.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/ILI9163C.o.d" -o ${OBJECTDIR}/ILI9163C.o ILI9163C.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
+${OBJECTDIR}/_ext/2112325346/sys_devcon.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
+${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
 else
 ${OBJECTDIR}/_ext/639803181/sys_clk_static.o: ../src/system_config/default/framework/system/clk/src/sys_clk_static.c  nbproject/Makefile-${CND_CONF}.mk
@@ -221,18 +221,6 @@ ${OBJECTDIR}/_ext/1360937237/main.o: ../src/main.c  nbproject/Makefile-${CND_CON
 	@${RM} ${OBJECTDIR}/_ext/1360937237/main.o 
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1360937237/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/1360937237/main.o.d" -o ${OBJECTDIR}/_ext/1360937237/main.o ../src/main.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
-${OBJECTDIR}/_ext/2112325346/sys_devcon.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
-	
-${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d 
-	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o 
-	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
-	
 ${OBJECTDIR}/i2c_master_noint.o: i2c_master_noint.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/i2c_master_noint.o.d 
@@ -244,6 +232,18 @@ ${OBJECTDIR}/ILI9163C.o: ILI9163C.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/ILI9163C.o.d 
 	@${RM} ${OBJECTDIR}/ILI9163C.o 
 	@${FIXDEPS} "${OBJECTDIR}/ILI9163C.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/ILI9163C.o.d" -o ${OBJECTDIR}/ILI9163C.o ILI9163C.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
+${OBJECTDIR}/_ext/2112325346/sys_devcon.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
+	
+${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o: ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/2112325346" 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d 
+	@${RM} ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o 
+	@${FIXDEPS} "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -ffunction-sections -O1 -I"../src" -I"../src/system_config/default" -I"../src/default" -I"../../../../../../microchip/harmony/v1_11/framework" -I"../src/system_config/default/framework" -MMD -MF "${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o.d" -o ${OBJECTDIR}/_ext/2112325346/sys_devcon_pic32mx.o ../../../../../../microchip/harmony/v1_11/framework/system/devcon/src/sys_devcon_pic32mx.c    -DXPRJ_default=$(CND_CONF)  -no-legacy-libc  $(COMPARISON_BUILD) 
 	
 endif
 
--- HW8/HW8_2/firmware/src/app.c
+++ HW8/HW8_2/firmware/src/app.c
@@ -82,6 +82,25 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 APP_DATA appData;
 
+#define SLAVE_ADDR 0b1101010
+#define CTRL1_XL 0x10
+#define CTRL2_G 0x11
+#define CTRL3_C 0x12
+#define OUT_TEMP_L 0x20
+#define OUTZ_H_XL 0x2D
+#define WHO_AM_I 0x0F
+
+short temperature;
+short gyroX;
+short gyroY;
+short gyroZ;
+short accelX;
+short accelY;
+short accelZ;
+
+char msg[100] ;
+char data[14];
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Callback Functions
@@ -98,9 +117,127 @@ APP_DATA appData;
 // *****************************************************************************
 
 
-/* TODO:  Add any necessary local functions.
-*/
+void setupIMU() {
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1); // slave address + write bit
+    i2c_master_send(CTRL1_XL);
+    i2c_master_send(0b10000010); 
+    i2c_master_stop();
+    
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1);
+    i2c_master_send(CTRL2_G);
+    i2c_master_send(0b10001000);
+    i2c_master_stop();
+    
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1);
+    i2c_master_send(CTRL3_C);
+    i2c_master_send(0b00000100);
+    i2c_master_stop();
+    
+    
+    //i2c_master_start();
+    //i2c_master_send(SLAVE_ADDR << 1); // indicate a write, send slave address
+    //i2c_master_send(GPIO);
+    //i2c_master_send(0b00000001); // turn on pin 0
+    //i2c_master_stop();
+}
 
+char read_register(unsigned char register_addr) {
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1 | 0); // indicate a write
+    i2c_master_send(register_addr);
+    
+    i2c_master_restart();
+    i2c_master_send((SLAVE_ADDR << 1) | 1); // send slave address, left shifted by 1,
+                                            // and then a 1 in lsb, indicating read
+    char master_read = i2c_master_recv();       // receive a byte from the bus
+    i2c_master_ack(1);                      // send NACK (1):  master needs no more bytes
+    i2c_master_stop();   
+    return master_read;
+}
+
+void I2C_read_multiple(unsigned char address, unsigned char register_addr, unsigned char* data, int length) {
+    
+    i2c_master_start();
+    i2c_master_send(address << 1); // indicate write
+    i2c_master_send(register_addr);
+    
+    i2c_master_restart();
+    i2c_master_send((address << 1) | 1); // send slave address, left shifted by 1,
+                                            // and then a 1 in lsb, indicating read
+    
+    int i =0;
+    for (i=0; i < length; i ++) {
+    
+        data[i] = i2c_master_recv();       // receive a byte from the bus
+        if (i == length-1) {
+            i2c_master_ack(1); 
+        } else {
+            i2c_master_ack(0); // let the chip know we want to keep reading  
+        }
+            
+    }
+    
+    //i2c_master_ack(1);   // send NACK (1):  master needs no more bytes
+    i2c_master_stop();
+}
+
+void draw_ascii(char c, unsigned short x, unsigned short y, unsigned short fgColor, unsigned short bgColor) {
+    char d = c - 0x20;
+    int col = 0;
+    int row = 0;
+    for (col = 0; col < 5; col++) {
+        if (x + col < 128) { // bounds check
+            char c = 0x01;
+            for (row = 0; row < 8; row++) {
+                if (y+row < 128) {
+                    //            LCD_drawPixel(x+col,y+row,0xffff);
+                    if ((ASCII[d][col] >> row) & 1) {
+                        LCD_drawPixel(x + col, y + row, fgColor);
+                    } else {
+                        LCD_drawPixel(x + col, y + row, bgColor);
+                    }
+                }
+            }
+        } 
+    }
+}
+
+void draw_string(char* msg, unsigned short x, unsigned short y, unsigned short fgColor, unsigned short bgColor)
+{
+     // can put some black spaces after hello to erase stuff
+
+    // loop through every element of msg
+    int i = 0;
+    while (msg[i] != 0) {// sprintf leaves a null character
+        draw_ascii(msg[i], x+5*i+1, y, fgColor, bgColor);
+        i++;
+    }
+}
+
+void draw_progress_bar(signed short percentage, unsigned short y, unsigned short height, unsigned short fgColor, unsigned short bgColor)
+{
+    float startPos = percentage * 128.0/100.0/2.0 +64;
+    float endPos;
+    endPos = (percentage <0) ? 64 : startPos;
+    startPos = (percentage<0) ? startPos : 64;
+    
+    
+    int col = 64;
+    int row = y;
+    for (col = 0; col < 128; col ++) {
+        
+       for (row = y; row < y + height; row ++) {
+            if (col >= startPos && col <= endPos) {
+                LCD_drawPixel(col, row, fgColor);
+            } else {
+                LCD_drawPixel(col, row, bgColor);
+            }
+        }
+    }
+}
 
 // *****************************************************************************
 // *****************************************************************************
@@ -126,10 +263,21 @@ void APP_Initialize ( void )
      * parameters.
      */
     
-    // make A4 an output pin
-    TRISAbits.TRISA4 = 0;
-    // set A4 to high
-    LATAbits.LATA4 = 1;
+//    // make A4 an output pin
+//    TRISAbits.TRISA4 = 0;
+//    // set A4 to high
+//    LATAbits.LATA4 = 1;
+    
+    //i2c
+    ANSELBbits.ANSB2 = 0;
+    ANSELBbits.ANSB3 = 0;  
+    i2c_master_setup();                       // init I2C2, which we use as a master
+
+    
+    SPI1_init();
+    LCD_init();
+    LCD_clearScreen(WHITE);
+    setupIMU();
 }
 
 
@@ -155,7 +303,11 @@ void APP_Tasks ( void )
         
             if (appInitialized)
             {
-            
+                I2C_read_multiple(SLAVE_ADDR, WHO_AM_I, data, 2);
+
+                sprintf(msg, "WHOAMI: %d", data[0]);
+                draw_string(msg,40,30,BLUE, WHITE);
+                
                 appData.state = APP_STATE_SERVICE_TASKS;
             }
             break;
@@ -163,21 +315,75 @@ void APP_Tasks ( void )
 
         case APP_STATE_SERVICE_TASKS:
         {
-            // blink code
+//            // blink code
+//            _CP0_SET_COUNT(0);
+//            
+//            LATAbits.LATA4 = 1;
+//            
+//            while (_CP0_GET_COUNT() < 12000) {
+//                
+//            }
+//            
+//            LATAbits.LATA4 = 0;
+//            
+//            while (_CP0_GET_COUNT() < 24000) {
+//                
+//            }
+            
             _CP0_SET_COUNT(0);
-            
-            LATAbits.LATA4 = 1;
-            
-            while (_CP0_GET_COUNT() < 12000) {
-                
-            }
-            
-            LATAbits.LATA4 = 0;
-            
-            while (_CP0_GET_COUNT() < 24000) {
-                
-            }
-            
+        
+            while (_CP0_GET_COUNT() < 480000000/2/5/20) {}
+            // code for debugging, should print 105 to the screen if connection working
+            I2C_read_multiple(SLAVE_ADDR, WHO_AM_I, data, 2);
+
+            sprintf(msg, "WHOAMI: %d", data[0]);
+            draw_string(msg,40,30,BLUE, WHITE);
+
+            I2C_read_multiple(SLAVE_ADDR, OUT_TEMP_L, data, 14);
+
+            // convert data to shorts
+            temperature = (data[1] << 8) | data[0];
+            gyroX = (data[3] << 8) | data[2];
+            gyroY = (data[5] << 8) | data[4];
+            gyroZ = (data[7] << 8) | data[6];
+            accelX = (data[9] << 8) | data[8];
+            accelY = (data[11] << 8) | data[10];
+            accelZ = (data[13] << 8) | data[12];
+
+            gyroX = gyroX * 35 ; // 1000 dps sensitivity; mdps/LSB
+            gyroY = gyroY * 35 ;
+            gyroZ = gyroZ * 35 ;
+
+            accelX = accelX * 0.061  ;// 2g sensitivity ; units are mg
+            accelY = accelY * 0.061 ;
+            accelZ = accelZ * 0.061 ;
+
+            //sprintf(msg, "T: %3.3f", (float) temperature);
+            //draw_string(msg,40,50,BLUE, WHITE);
+
+    //        sprintf(msg, "X: %3.3f", (float)gyroX);
+    //        draw_string(msg,40,60,RED, WHITE);
+    //        
+    //        sprintf(msg, "Y: %3.3f", (float)gyroY);
+    //        draw_string(msg,40,70,RED, WHITE);
+    //        
+    //        sprintf(msg, "Z: %3.3f", (float)gyroZ);
+    //        draw_string(msg,40,80,RED, WHITE);
+    //        
+    //        sprintf(msg, "aX: %3.3f", (float)accelX);
+    //        draw_string(msg,40,90,BLACK, WHITE);
+    //        
+    //        sprintf(msg, "aY: %3.3f", (float)accelY);
+    //        draw_string(msg,40,100,BLACK, WHITE);
+    //        
+    //        sprintf(msg, "aZ: %3.3f", (float)accelZ);
+    //        draw_string(msg,40,110,BLACK, WHITE);
+
+
+
+            draw_progress_bar(accelX/2.0,50,2,RED,WHITE);
+
+            draw_progress_bar(accelY/2.0,70,2,BLUE,WHITE);
             break;
         }
 
