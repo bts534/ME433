67dfeb7b1560439f499a1a75d192666fbc070b48
Brian Soetikno <brian.soetikno@northwestern.edu>
2017-06-06 14:00:27 -0500

    Finished HW 14: CDC and Android USB communication

---
+++ HW14/HW14/firmware/HW19.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1360937237/main.o:  \
+../src/main.c  \
+ ../../../../../../microchip/harmony/v1_11/framework/system/common/sys_module.h  \
+ ../../../../../../microchip/harmony/v1_11/framework/system/common/sys_common.h 
